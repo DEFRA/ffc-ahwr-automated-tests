cc57ebd546439a1c9a30b0154f57a3817336ad90
Rob Catton <robmcatton@gmail.com>
2025-03-13 14:26:47 +0000

    2 script start

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -3,7 +3,7 @@
 set -e
 set -x
 
-# node --version
+node --version
 
 # NODE_VERSION="20.18.1"
 # NVM_DIR="$HOME/.nvm"
@@ -16,14 +16,14 @@ export NVM_DIR="/var/lib/jenkins/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
-nvm install 20.18.1
-nvm use 20.18.1
-nvm alias default 20.18.1
+# nvm install 20.18.1
+# nvm use 20.18.1
+# nvm alias default 20.18.1
 
-echo "Node version: $(node -v)"
-echo "NPM version: $(npm -v)"
+# echo "Node version: $(node -v)"
+# echo "NPM version: $(npm -v)"
 
-npm install --omit=dev --ignore-scripts
+# npm install --omit=dev --ignore-scripts
 
 
 # mkdir -p "$NVM_DIR"
