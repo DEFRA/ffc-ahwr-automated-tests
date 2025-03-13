8d7a6b708173bf73605ce9eb00753d9d64dd50fb
Rob Catton <robmcatton@gmail.com>
2025-03-13 15:15:46 +0000

    .

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -10,8 +10,9 @@ export NVM_DIR="/var/lib/jenkins/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 
-sudo apt update
-sudo apt upgrade libc6
+sudo su - 
+apt update
+apt upgrade libc6
 
 nvm install 20.18.1
 nvm use 20.18.1
