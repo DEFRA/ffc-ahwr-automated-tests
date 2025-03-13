a2368b98632c82fec0968cbcc62b024bd6fdc479
Rob Catton <robmcatton@gmail.com>
2025-03-13 13:54:19 +0000

    Attempting to use nvm already installed on jenkins

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -2,29 +2,44 @@
 
 set -e
 
-node --version
+# node --version
 
-NODE_VERSION="20.18.1"
-NVM_DIR="$HOME/.nvm"
+# NODE_VERSION="20.18.1"
+# NVM_DIR="$HOME/.nvm"
 
 # sudo apt-get update && sudo apt-get install -y curl bash
 # sudo touch ~/.bash_profile
 
-mkdir -p "$NVM_DIR"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-. "$NVM_DIR/nvm.sh"
 
-nvm install "$NODE_VERSION"
-nvm use "$NODE_VERSION"
-nvm alias default "$NODE_VERSION"
+export NVM_DIR="/var/lib/jenkins/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Load nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # Load nvm bash_completion
 
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/node" /usr/local/bin/node
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npm" /usr/local/bin/npm
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npx" /usr/local/bin/npx
+nvm install 20.18.1
+nvm use 20.18.1
+nvm alias default 20.18.1
 
-command -v node && node -v
-command -v npm && npm -v
-command -v npx && npx -v
+echo "Node version: $(node -v)"
+echo "NPM version: $(npm -v)"
 
 npm install --omit=dev --ignore-scripts
+
+
+# mkdir -p "$NVM_DIR"
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+# export NVM_DIR="$HOME/.nvm"
+# . "$NVM_DIR/nvm.sh"
+
+# nvm install "$NODE_VERSION"
+# nvm use "$NODE_VERSION"
+# nvm alias default "$NODE_VERSION"
+
+# ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/node" /usr/local/bin/node
+# ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npm" /usr/local/bin/npm
+# ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npx" /usr/local/bin/npx
+
+# command -v node && node -v
+# command -v npm && npm -v
+# command -v npx && npx -v
+
+# npm install --omit=dev --ignore-scripts
