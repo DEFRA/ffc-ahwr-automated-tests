35b24cf5f6ef12b24c936c4ae91ff66450704d98
Rob Catton <robmcatton@gmail.com>
2025-03-13 12:12:44 +0000

    Moving back to curling

--- scripts/setup_node.sh
+++ scripts/setup_node.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+
 set -e
 
-NODE_VERSION=20.18.1
+node --version
+
+NODE_VERSION="20.18.1"
+NVM_DIR="$HOME/.nvm"
+
+sudo apt-get update && sudo apt-get install -y curl bash
+
+mkdir -p "$NVM_DIR"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-
-# sudo apt-get update && sudo apt-get install -y curl bash
-
-if [ ! -d "$NVM_DIR" ]; then
-  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout v0.39.0
-fi
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+. "$NVM_DIR/nvm.sh"
 
 nvm install "$NODE_VERSION"
 nvm use "$NODE_VERSION"
 nvm alias default "$NODE_VERSION"
 
-# Ensure Node.js and npm are available
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/node" "$HOME/bin/node"
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npm" "$HOME/bin/npm"
-ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npx" "$HOME/bin/npx"
+ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/node" /usr/local/bin/node
+ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npm" /usr/local/bin/npm
+ln -sf "$NVM_DIR/versions/node/v$NODE_VERSION/bin/npx" /usr/local/bin/npx
 
-# Verify installation
-node -v
-npm -v
+command -v node && node -v
+command -v npm && npm -v
+command -v npx && npx -v
+
+npm install --omit=dev --ignore-scripts
