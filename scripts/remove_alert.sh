664475cfa490f61e113010eae4d2d0632bf7be26
Ben Hope <bendef24@gmail.com>
2025-03-26 09:42:59 +0000

    feat(AHWR-621): alert when pipeline fails

--- scripts/remove_alert.sh
+++ scripts/remove_alert.sh
@@ -5,6 +5,7 @@ AHWR_BRANCH="$2"
 
 echo "🧽 Pre-run Cleanup: Remove Alert..."
 
+echo "Removing key 'service_build_tests_failed_$AHWR_BRANCH' from storage account table ffcahwrjenkinsfailures.."
 az storage entity delete \
  --connection-string $AZURE_STORAGE_CONNECTION_STRING_JENKINS_FAILURES \
  --table-name ffcahwrjenkinsfailures \
--- scripts/send_alert.sh
+++ scripts/send_alert.sh
@@ -6,6 +6,7 @@ AHWR_RUN=$3
 
 echo "🚨 Trigger Alert..."
 
+echo "Inserting key 'service_build_tests_failed_$AHWR_BRANCH' into storage account table ffcahwrjenkinsfailures.."
 az storage entity insert \
  --connection-string $AZURE_STORAGE_CONNECTION_STRING_JENKINS_FAILURES \
  --table-name ffcahwrjenkinsfailures \
