8a7c0d6e5104f14ae17498c3d9ac0acd9ef0fe67
berlin-ee <berlin.bernadin@equalexperts.com>
2025-04-08 20:48:40 +0100

    test(AHWR-244): Adding one backoffice test and updating with latest DB changelog

--- test/utils/selectors.js
+++ test/utils/selectors.js
@@ -30,6 +30,29 @@ export const SUBMISSION_PANEL_TITLE = ".govuk-panel__title";
 
 export const START_NEW_CLAIM_BUTTON = "#start";
 
+export const BO_AGREEMENTS_TAB = 'a.govuk-tabs__list-item[href="/agreements"]';
+
+export const BO_AGREEMENT_NUMBER_SELECTOR = ".govuk-panel__body strong";
+
+export const BO_VIEW_CLAIMS_LINK_SELECTOR = "=View claims";
+
+export const BO_CHECKED_AGAINST_CHECKLIST_SELECTOR =
+  'input[type="checkbox"][value="checkedAgainstChecklist"]';
+
+export const BO_SENT_CHECKLIST_SELECTOR =
+  'input[type="checkbox"][value="sentChecklist"]';
+
+export const BO_CONFIRM_AND_CONTINUE_BUTTON = "button=Confirm and continue";
+
+export const BO_CLAIM_STATUS_TEXT_SELECTOR = ".govuk-tag--orange";
+
+export const BO_BACK_TO_ALL_CLAIMS_SELECTOR = 'a[href="/claims"]';
+
+export const BO_RECOMMEND_TO_PAY_BUTTON = 'a[href*="recommendToPay"]';
+
+export const BO_CLAIMS_MAIN_HEADING_SELECTOR =
+  "#main-content h1.govuk-heading-xl";
+
 export function getConfirmCheckDetailsSelector(value) {
   return `input[name="confirmCheckDetails"][value="${value}"]`;
 }
@@ -76,4 +99,12 @@ export function getTestResultsSelector(value) {
 
 export function getBiosecuritySelector(value) {
   return `input[name="biosecurity"][value="${value}"]`;
+}
+
+export function getAgreementNumberSelector(agreementNumber) {
+  return `td[data-sort-value="${agreementNumber}"]`;
+}
+
+export function getViewClaimLinkSelector(claimNumber) {
+  return `a[href*="${claimNumber}"]`;
 }
