f6388e4f8260e430fff25bdec1b4a7cf0008e34d
Rob Catton <robmcatton@gmail.com>
2025-06-09 13:30:43 +0100

    Added new backoffice tests

--- test/utils/common.js
+++ test/utils/common.js
@@ -10,6 +10,18 @@ import {
   getWhenTestingWasCarriedOutSelector,
   getSheepTestsDiseaseConditionSelector,
   START_NEW_CLAIM_BUTTON,
+  AGREEMENT_NUMBER_SELECTOR,
+  TERMS_AND_CONDITIONS_CHECKBOX,
+  NUMBER_OF_ANIMALS_TESTED,
+  VETS_NAME,
+  VET_RCVS_NUMBER,
+  LABORATORY_URN,
+  SUBMIT_CLAIM_BUTTON,
+  REFERENCE,
+  getTypeOfLivestockSelector,
+  getTypeOfReviewSelector,
+  getSpeciesNumbersSelector,
+  getConfirmCheckDetailsSelector,
 } from "./selectors.js";
 
 export function getDevSignInUrl(type) {
@@ -105,4 +117,49 @@ export async function clickSubmitButton() {
 
 export async function clickStartNewClaimButton() {
   await $(START_NEW_CLAIM_BUTTON).click();
+}
+
+export async function createAgreement(sbi, browser) {
+  await browser.url(getDevSignInUrl("apply"));
+  await fillAndSubmitSBI(sbi);
+  await $(getConfirmCheckDetailsSelector("yes")).click();
+  await clickSubmitButton();
+  await clickSubmitButton();
+  await clickSubmitButton();
+  await clickSubmitButton();
+  await $(TERMS_AND_CONDITIONS_CHECKBOX).click();
+  await clickSubmitButton();
+  await verifySubmission("Application complete");
+  const agreementNumber = (await $(AGREEMENT_NUMBER_SELECTOR).getText()).trim();
+
+  return agreementNumber;
+}
+
+export async function createClaim(sbi, browser) {
+  await browser.url(getDevSignInUrl("claim"));
+  await fillAndSubmitSBI(sbi);
+  await $(getConfirmCheckDetailsSelector("yes")).click();
+  await clickSubmitButton();
+  await clickStartNewClaimButton();
+  await clickOnElementAndContinue(getTypeOfLivestockSelector("sheep"));
+  await clickOnElementAndContinue(getTypeOfReviewSelector("review"));
+  await enterVisitDateAndContinue();
+  await enterWhenTestingWasCarriedOutAndContinue("whenTheVetVisitedTheFarmToCarryOutTheReview");
+  await clickOnElementAndContinue(getSpeciesNumbersSelector("yes"));
+  await fillInputAndContinue(NUMBER_OF_ANIMALS_TESTED, "10");
+  await fillInputAndContinue(VETS_NAME, "Mr Auto Test");
+  await fillInputAndContinue(VET_RCVS_NUMBER, "1234567");
+  await fillInputAndContinue(LABORATORY_URN, "sh-rr-534346");
+  await $(SUBMIT_CLAIM_BUTTON).click();
+  await verifySubmission("Claim complete");
+  await expect($(REFERENCE)).toHaveText(expect.stringContaining("RESH"));
+  const claimNumber = await $(REFERENCE).getText();
+
+  return claimNumber;
+}
+
+export async function swapBackOfficeUser(userName, browser) {
+  const backOfficeClaimsRoute = getDevSignInUrl("backoffice");
+  const loginRoute = backOfficeClaimsRoute.replace("claims", `login?userId=${userName}`);
+  await browser.url(loginRoute);
 }
--- test/utils/constants.js
+++ test/utils/constants.js
@@ -1,8 +1,13 @@
 export const APPLY_REVIEW_CLAIM_SBI = "107167406";
 export const SHEEP_ENDEMIC_CLAIM_SBI = "106208072";
 export const BEEF_ENDEMIC_CLAIM_SBI = "107085418";
-export const BACK_OFFICE_SBI = "106825898";
-export const BACK_OFFICE_FLAG_SBI = "106592443";
+export const BACK_OFFICE_APPROVE_SBI = "106825898";
+export const BACK_OFFICE_REJECT_SBI = "106215898";
+export const BACK_OFFICE_ON_HOLD_SBI = "104215119";
 export const DASHBOARD_SBI = "107019440";
 export const JOHNES_DISEASE = "johnes";
 export const MULTIPLE_HERDS_SBI = "123454321";
+
+// Below references from changelog/insert_on_hold_claim.sql
+export const ON_HOLD_AGREEMENT_REF = "IAHW-SCV6-E55L";
+export const ON_HOLD_CLAIM_REF = "REPI-UG9L-I1XP";
